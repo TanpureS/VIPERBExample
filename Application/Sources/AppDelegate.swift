1008405321e5380b13dc05d8ce1d7abf3634162b
Shivaji Tanpure <shivajitanpure@Atoss-Mac-mini.local>
2018-07-02 19:59:42 +0530

    initial commit

---
+++ Application/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  viperb
+//
+//  Created by Tibor Bödecs on 2018. 03. 08..
+//  Copyright © 2018. Tibor Bödecs. All rights reserved.
+//
+
+import UIKit
+//Link: https://theswiftdev.com/2018/03/12/the-ultimate-viper-architecture-tutorial/
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
